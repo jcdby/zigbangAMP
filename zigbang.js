9c0cde3b11c8b638691f2acef8bba348ae09be97
jcdby520@gmail.com <jcdby520@gmail.com>
2016-12-09 23:13:25 +0900

    add modal inplemented by myself

--- zigbang.js
+++ zigbang.js
@@ -1,3 +1,17 @@
-function alertMe() {
-  alert('good job!');
+
+function showModal() {
+  modal.style.display = "block";
+  b.setAttribute('class', 'modal-open')
 }
+
+function closeModal(){
+  modal.style.display = "none";
+  b.classList.remove('modal-open')
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        closeModal()
+    }
+}
+
